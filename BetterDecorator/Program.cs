134136d1d26db03ca34bae41f4fe5cd0d999a794
Clara Antunes <claraantunes.work@gmail.com>
2024-03-13 16:50:30 +0000

    Add XML Comments to Decor method

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -5,13 +5,22 @@ namespace BetterDecorator
     class Program
     {
 
+        /// <summary>
+        /// Método que decora uma string com um caractere repetido uma certa 
+        /// quantidade de vezes.
+        /// </summary>
+        /// <param name="s">String que vai ser decorada.</param>
+        /// <param name="dec">Caractere que vai ser usado pra decorar.</param>
+        /// <param name="rep">Vezes que o caractere é repetido.</param>
+        /// <returns></returns>
         private static string Decor(string s, char dec, int rep)
         {
             string decor = new string(dec, rep);
             return $"{decor} {s} {decor}";
         }
+    
         static void Main(string[] args)
-        {
+        {   
             string input = args[0];
             char decorchar = args[1][0];
             int repcount = int.Parse(args[2]);
