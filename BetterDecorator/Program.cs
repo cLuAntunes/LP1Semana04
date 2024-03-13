5e26f972cd7170692fb8651f2c1ae842c03e674c
Clara Antunes <claraantunes.work@gmail.com>
2024-03-13 16:29:35 +0000

    Add rep parameter to Decor method

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+
+        private static string Decor(string s, char dec, int rep)
+        {
+            string decor = new string(dec, rep);
+            return $"{decor} {s} {decor}";
+        }
+        static void Main(string[] args)
+        {
+            string input = args[0];
+            char decorchar = args[1][0];
+            int repcount = int.Parse(args[2]);
+
+            string decorated = Decor(input, decorchar, repcount);
+            Console.WriteLine(decorated);
+        }
+    }
+}
