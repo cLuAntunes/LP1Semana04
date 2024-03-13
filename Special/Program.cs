dc6e73b8af6f9e051bc62192798936584dcdee32
Clara Antunes <claraantunes.work@gmail.com>
2024-03-13 19:15:41 +0000

    Add GetSpecial method

--- Special/Program.cs
+++ Special/Program.cs
@@ -3,10 +3,21 @@
 namespace Special
 {
     class Program
-    {
+    {   
+        private static int GetSpecial(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1 || n == 2)
+                return 1;
+            else
+                return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(GetSpecial(0));
+            Console.WriteLine(GetSpecial(2));
+            Console.WriteLine(GetSpecial(7));
         }
     }
 }
